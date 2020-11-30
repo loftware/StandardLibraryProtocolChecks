529dd4550fd3a399984646857ce5c16261ebf1ae
Dave Abrahams <dabrahams@google.com>
2020-11-29 16:43:00 -0800

    Add some Comparable testing.

--- Tests.swift
+++ Tests.swift
@@ -29,42 +29,51 @@ extension Box: Comparable where Content: Comparable {
   }
 }
 
-final class Flawed {
-  let value: Float
-  let flaw: Flaw
-
-  init(_ value: Float, flaw: Flaw) { (self.value, self.flaw) = (value, flaw) }
+/// A type that selectively breaks one law of regular types.
+final class BrokenFloat {
+  enum Law {
+    case equatable(EquatableLaw), hashable(HashableLaw), comparable(ComparableLaw)
+  }
   
-  enum Flaw {
-    case equatableReflexivity, equatableSymmetry, equatableTransitivity
-    case hashableEquatableConsistency
+  let value: Float  
+  let brokenLaw: Law
+
+  init(_ value: Float, butNot brokenLaw: Law) {
+    self.value = value
+    self.brokenLaw = brokenLaw
   }
+  
 }
 
-extension Flawed: Equatable {
-  static func == (l: Flawed, r: Flawed) -> Bool {
-    l.flaw == .equatableReflexivity ? l !== r && l.value == r.value
-      : l.flaw == .equatableSymmetry ? l.value <= r.value
-      : l.flaw == .equatableTransitivity ? abs(l.value - r.value) <= 1
-      : l.value == r.value
-  }
+//
+// - MARK: Equatable
+//
+
+extension BrokenFloat {
+  enum EquatableLaw: String { case equalIsReflexive, equalIsSymmetric, equalIsTransitive }
 }
 
-extension Flawed: Hashable {
-  func hash(into sink: inout Hasher) {
-    if flaw == .hashableEquatableConsistency {
-      ObjectIdentifier(self).hash(into: &sink)
-    }
-    else {
-      value.hash(into: &sink)
+extension BrokenFloat: Equatable {
+  static func == (l: BrokenFloat, r: BrokenFloat) -> Bool {
+    guard case .equatable(let broken) = l.brokenLaw else { return l.value == r.value }
+    switch broken {
+    case .equalIsReflexive: return l !== r && l.value == r.value
+    case .equalIsSymmetric: return l.value <= r.value
+    case .equalIsTransitive: return abs(l.value - r.value) <= 1
     }
   }
 }
 
 final class EquatableTests: CheckXCAssertionFailureTestCase {
-  static var irreflexiveSamples = [0, 0, 0].map { Flawed($0, flaw: .equatableReflexivity) }
-  static var asymmetricSamples = [0, 1, 2].map { Flawed($0, flaw: .equatableSymmetry) }
-  static var intransitiveSamples = [0, 1, 2].map { Flawed($0, flaw: .equatableTransitivity) }
+  static var irreflexiveSamples = [0, 0, 0, 10, 100].map {
+    BrokenFloat($0, butNot: .equatable(.equalIsReflexive))
+  }
+  static var asymmetricSamples = [0, 1, 2, 10, 100].map {
+    BrokenFloat($0, butNot: .equatable(.equalIsSymmetric))
+  }
+  static var intransitiveSamples = [0, 1, 2, 10, 100].map {
+    BrokenFloat($0, butNot: .equatable(.equalIsTransitive))
+  }
   
   func testIndistinctInstances() {
     0.checkEquatableSemantics()
@@ -104,6 +113,26 @@ final class EquatableTests: CheckXCAssertionFailureTestCase {
   }
 }
 
+//
+// - MARK: Hashable
+//
+
+extension BrokenFloat {
+  enum HashableLaw { case hashValueIsConsistentWithEquality }
+}
+
+extension BrokenFloat: Hashable {
+  func hash(into sink: inout Hasher) {
+    if case .hashable(let broken) = brokenLaw {
+      switch broken {
+      case .hashValueIsConsistentWithEquality: ObjectIdentifier(self).hash(into: &sink)
+      }
+    } else {
+      value.hash(into: &sink)
+    }
+  }
+}
+
 class HashableTests: CheckXCAssertionFailureTestCase {
   func testIndistinctInstances() {
     0.checkHashableSemantics()
@@ -115,6 +144,7 @@ class HashableTests: CheckXCAssertionFailureTestCase {
     Box(1).checkHashableSemantics(equal: Box(1), Box(1))
   }
 
+  /// Shows that testing Hashable also tests Equatable
   func testEquatableFailures() {
     let r = EquatableTests.irreflexiveSamples
     checkXCAssertionFailure(r[0].checkHashableSemantics(equal: r[1], r[2]), "reflexivity")
@@ -127,12 +157,118 @@ class HashableTests: CheckXCAssertionFailureTestCase {
   }
 
   func testHashableFailure() {
-    let s = [0, 0, 0].map { Flawed($0, flaw: .hashableEquatableConsistency) }
+    let s = [0, 0, 0].map { BrokenFloat($0, butNot: .hashable(.hashValueIsConsistentWithEquality)) }
     
     s[0].checkEquatableSemantics(equal: s[1], s[2])
     
     checkXCAssertionFailure(
       s[0].checkHashableSemantics(equal: s[1], s[2]), "distinct hash")
+  }
+}
+
+//
+// - MARK: Comparable
+//
+
+extension BrokenFloat {
+  // There are many laws because for some reason all the comparison operators are separate
+  // requirements, even though they can be derived from `<`.
+  enum ComparableLaw {    
+    case less(LessLaw)
+    case lessOrEqual(LessOrEqualLaw)
+    case greaterOrEqual(GreaterOrEqualLaw)
+    case greater(GreaterLaw)
+  }
+  enum LessLaw { case equalImpliesFalse, greaterOrEqualImpliesFalse, greaterImpliesFalse }
+  enum LessOrEqualLaw { case lessImpliesTrue, equalImpliesTrue, greaterImpliesFalse }
+  enum GreaterOrEqualLaw { case lessImpliesFalse, equalImpliesTrue, greaterImpliesTrue }
+  enum GreaterLaw { case lessImpliesFalse, lessOrEqualImpliesFalse, equalImpliesFalse }
+}
+
+extension BrokenFloat: Comparable {
+  static func < (l: BrokenFloat, r: BrokenFloat) -> Bool {
+    if case .comparable(.less(let broken)) = l.brokenLaw {
+      switch broken {
+      case .equalImpliesFalse: if l == r { return true }
+      case .greaterOrEqualImpliesFalse: if l >= r { return true }
+      case .greaterImpliesFalse: if l > r { return true }
+      }
+    }
+    return l.value < r.value
+  }
+
+  static func <= (l: BrokenFloat, r: BrokenFloat) -> Bool {
+    if case .comparable(.lessOrEqual(let broken)) = l.brokenLaw {
+      switch broken {
+      case .lessImpliesTrue: if l < r { return false }
+      case .equalImpliesTrue: if l == r { return false }
+      case .greaterImpliesFalse: if l > r { return true }
+      }
+    }
+    return l.value <= r.value
+  }
+
+  static func >= (l: BrokenFloat, r: BrokenFloat) -> Bool {
+    if case .comparable(.greaterOrEqual(let broken)) = l.brokenLaw {
+      switch broken {
+      case .lessImpliesFalse: if l < r { return true }
+      case .equalImpliesTrue: if l == r { return false }
+      case .greaterImpliesTrue: if l > r { return false }
+      }
+    }
+    return l.value >= r.value
+  }
+
+  static func > (l: BrokenFloat, r: BrokenFloat) -> Bool {
+    if case .comparable(.greater(let broken)) = l.brokenLaw {
+      switch broken {
+      case .lessImpliesFalse: if l < r { return true }
+      case .lessOrEqualImpliesFalse: if l <= r { return true }
+      case .equalImpliesFalse: if l == r { return true }
+      }
+    }
+    return l.value > r.value
+  }
+}
+
+class ComparableTests: CheckXCAssertionFailureTestCase {
+  func testIndistinctInstances() {
+    0.checkComparableSemantics(greater: 1, greaterStill: 2)
+    (-2).checkComparableSemantics(greater: -1, greaterStill: 0)
+  }
+  
+  func testDistinctInstances() {
+    let box0 = Box(0), box0a = Box(0), box0b = Box(0)
+    let box1 = Box(1), box1a = Box(1), box1b = Box(1)
+    let box2 = Box(2)
+    let box3 = Box(3)
+    box0.checkComparableSemantics(equal: box0a, box0b, greater: box1, greaterStill: box2)
+    box1.checkComparableSemantics(equal: box1a, box1b, greater: box2, greaterStill: box3)
+  }
+
+  /// Shows that testing Comparable also tests Equatable
+  func testEquatableFailures() {
+    let r = EquatableTests.irreflexiveSamples
+
+    checkXCAssertionFailure(
+      r[0].checkComparableSemantics(equal: r[1], r[2], greater: r[3], greaterStill: r[4]),
+      "reflexivity")
+
+    let s = EquatableTests.asymmetricSamples
+    checkXCAssertionFailure(
+      s[0].checkComparableSemantics(equal: s[1], s[2], greater: s[3], greaterStill: s[4]),
+      "symmetry")
+    
+    let t = EquatableTests.intransitiveSamples
+    checkXCAssertionFailure(
+      t[0].checkComparableSemantics(equal: t[1], t[2], greater: t[3], greaterStill: t[4]),
+      "transitivity")
+  }
+
+  func testComparableFailures() {
+    let s = [0, 0, 0].map { BrokenFloat($0, butNot: .hashable(.hashValueIsConsistentWithEquality)) }
+    
+    s[0].checkEquatableSemantics(equal: s[1], s[2])
   }
 }
 
